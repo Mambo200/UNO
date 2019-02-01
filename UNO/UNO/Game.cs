b4d3c5254e37f42ae0d0697673cb5487fdc5cf92
Tobias <tobis-stroedicke@gmx.de>
2019-02-01 16:23:39 +0100

    Player Class

--- UNO/UNO/Game.cs
+++ UNO/UNO/Game.cs
@@ -9,9 +9,56 @@ namespace UNO
     class Game
     {
         Stack<Card> allCards = new Stack<Card>();
+        Player[] players = new Player[2];
+        public Player currentPlayer;
+
+        /// <summary>
+        /// Start the Game
+        /// </summary>
         public void StartGame()
         {
-            allCards = Card.GiveDeck(false);
+            // generate Player
+            for (int i = 0; i < players.Length; i++)
+            {
+                players[i] = new Player(i + 1);
+            }
+            allCards = Card.GiveDeck(true);
+
+
+            Card.GiveCards(allCards, players);
+
+            // set player 1 as 
+            currentPlayer = players[0];
+
+            DoGame();
+        }
+
+        /// <summary>
+        /// Ingame function
+        /// </summary>
+        private void DoGame()
+        {
+            ShowCurrentPlayersCards();
+        }
+
+        /// <summary>
+        /// Show Players cards
+        /// </summary>
+        private void ShowCurrentPlayersCards()
+        {
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+            Console.Clear();
+
+            string s = currentPlayer.ToString() + "\n";
+            Console.WriteLine(s);
+
+            foreach (Card card in currentPlayer.CardHand)
+            {
+                Console.WriteLine(card);
+            }
+            Console.ReadKey();
+
         }
     }
 }
