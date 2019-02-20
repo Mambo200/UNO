e937032f7fac6e0c6989768bf3e1453984e505ce
DESKTOP-BC3V8EV\Tobias <tobis-stroedicke@gmx.de>
2019-02-20 22:37:18 +0100

    Reverse Fix TODO: helper class to set Names

---
+++ UNO/UNO/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNO
+{
+    public static class Helper
+    {
+        public static void SetName(Player _player)
+        {
+            string name = "";
+            Console.Clear();
+            Console.Write("Name (less or equal 8 characters): ");
+            Console.ReadLine();
+        }
+    }
+}
